e8ea60ade265a5d23f2f95065b5ae36ebfe39722
Khiem <phamxuankhiem1102@gmail.com>
2026-06-18 00:42:31 +0700

    Add PrepDb class for database seeding functionality

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,40 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd = false)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                if (context != null) {
+                    SeedData(context);
+                }                
+            }
+            
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }            
+        }
+    }
+
+}
