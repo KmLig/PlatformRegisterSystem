c48a930c1f08970ffffa200742baae1f3a90c7a8
Khiem <phamxuankhiem1102@gmail.com>
2026-06-22 23:30:14 +0700

    Remove weather forecast endpoint and related code from PlatformService for cleaner architecture

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -30,25 +30,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGet("/platforms",  (IPlatformRepo repo, PlatformMappers mapper) =>
 {
    IEnumerable<Platform> platforms = repo.GetAllPlatforms();
@@ -65,8 +46,3 @@ app.Lifetime.ApplicationStarted.Register(() =>
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
