43ecd9e4c91a6f795aac3d123ee29f7fabf59769
Khiem <phamxuankhiem1102@gmail.com>
2026-06-22 23:29:12 +0700

    Add MapperIgnoreTarget attribute to MapToModel method in PlatformMappers to exclude Platform.Id from mapping

--- PlatformService/Mappers/PlatformMappers.cs
+++ PlatformService/Mappers/PlatformMappers.cs
@@ -9,5 +9,6 @@ public partial class PlatformMappers
 {
     // Mapperly will generate the implementation for this method
     public partial PlatformReadDto MapToReadDto(Platform platform);
+    [MapperIgnoreTarget(nameof(Platform.Id))]
     public partial Platform MapToModel(PlatformCreateDto platformCreateDto);
 }
