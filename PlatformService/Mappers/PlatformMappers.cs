a47c92a44fb30ff17310ad727effe020c1eae3c4
Khiem <phamxuankhiem1102@gmail.com>
2026-06-22 22:33:26 +0700

    Add MapToModel method to PlatformMappers for mapping PlatformCreateDto to Platform model

--- PlatformService/Mappers/PlatformMappers.cs
+++ PlatformService/Mappers/PlatformMappers.cs
@@ -9,4 +9,5 @@ public partial class PlatformMappers
 {
     // Mapperly will generate the implementation for this method
     public partial PlatformReadDto MapToReadDto(Platform platform);
+    public partial Platform MapToModel(PlatformCreateDto platformCreateDto);
 }
